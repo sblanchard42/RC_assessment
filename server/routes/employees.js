dcf17370e0fe5fced0083f5db3f432f7cd710cbd
Sara Blanchard <sarablanchard42@gmail.com>
2023-05-14 15:05:31 -0700

    Adding a few server side files including package and package-lock.json

---
+++ server/routes/employees.js
@@ -0,0 +1,89 @@
+const express = require('express');
+
+const router = express.Router();
+const Employee = require('../models').Employee;
+const { asyncHandler } = require('../middleware/async-handler');
+
+// Return all employees
+router.get('/employees', asyncHandler(async (req, res) => {
+    let employees = await Employee.findAll({
+        attributes: {
+            exclude: ['createdAt', 'updatedAt']
+        }
+    });
+    res.json(employees);
+}));
+
+// Return a specific employee
+router.get('/employees/:personal_id', asyncHandler(async (req, res) => {
+    const employee = await Employee.findByPk(req.params.personal_id, {
+        attributes: {
+            exclude: ['createdAt', 'updatedAt']
+        }
+    });
+    if (employee) {
+        res.json(employee);
+    } else {
+        res.json({
+            "error": "Sorry, we couldn't find the employee you were looking for."
+        });
+    }
+}));
+
+// Create a employee
+router.post('/employees', asyncHandler(async (req, res) => {
+    try {
+        const newEmployee = await Employee.create(req.body);
+        res.status(201)
+            .location(`/employees/${newEmployee.dataValues.personal_id}`)
+            .end();
+    } catch (error) {
+        console.log('ERROR: ', error.name);
+        if (error.name === 'SequelizeValpersonal_idationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
+
+// Update an existing employee
+router.put("/employees/:personal_id", asyncHandler(async (req, res, next) => {
+    let employee;
+    try {
+        employee = await Employee.findByPk(req.params.personal_id);
+        if (employee) {
+
+            await employee.update(req.body);
+            res.status(204).end();
+
+
+        } else {
+            const err = new Error(`Employee Not Found`);
+            res.status(404).json({ error: err.message });
+        }
+    } catch (error) {
+        if (error.name === 'SequelizeValpersonal_idationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
+
+// Delete an existing employee
+router.delete("/employees/:personal_id", asyncHandler(async (req, res, next) => {
+    const employee = await Employee.findByPk(req.params.personal_id);
+    if (employee) {
+        await employee.destroy();
+        res.status(204).end();
+
+    } else {
+        const err = new Error(`Employee Not Found`);
+        res.status(404).json({ error: err.message });
+    }
+}));
+
+module.exports = router;
---
+++ server/seed/database.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const bcryptjs = require('bcryptjs');
+const Context = require('./context');
+
+class Database {
+    constructor(seedData, enableLogging) {
+        this.employees = seedData.employees;
+        this.enableLogging = enableLogging;
+        this.context = new Context('fsjstd-restapi.db', enableLogging);
+    }
+
+    log(message) {
+        if (this.enableLogging) {
+            console.info(message);
+        }
+    }
+
+    tableExists(tableName) {
+        this.log(`Checking if the ${tableName} table exists...`);
+
+        return this.context
+            .retrieveValue(`
+        SELECT EXISTS (
+          SELECT 1 
+          FROM sqlite_master 
+          WHERE type = 'table' AND name = ?
+        );
+      `, tableName);
+    }
+
+    createEmployee(employee) {
+        return this.context
+            .execute(`
+        INSERT INTO Employees
+          (personal_id, first_name, last_name, email_address, hire_date, job_title, agency_num, registration_date)
+        VALUES
+          (?, ?, ?, ?, ?, ?, ?, date());
+      `,
+                employee.personal_id,
+                employee.first_name,
+                employee.last_name,
+                employee.email_address,
+                employee.hire_date,
+                employee.job_title,
+                employee.agency_num,
+                employee.registration_date);
+    }
+
+    async createEmployees(employees) {
+        for (const employee of employees) {
+            await this.createEmployee(employee);
+        }
+    }
+
+    async init() {
+        const employeeTableExists = await this.tableExists('Employees');
+
+        if (employeeTableExists) {
+            this.log('Dropping the Employees table...');
+
+            await this.context.execute(`
+        DROP TABLE IF EXISTS Employees;
+      `);
+        }
+
+        this.log('Creating the Employees table...');
+
+        await this.context.execute(`
+      CREATE TABLE Employees (
+        personal_id INTEGER PRIMARY KEY NOT NULL,
+        first_name VARCHAR(50) NOT NULL DEFAULT '',
+        last_name VARCHAR(50) NOT NULL DEFAULT '',
+        email_address VARCHAR(100) NOT NULL DEFAULT '',
+        hire_date DATE NOT NULL DEFAULT '',
+        job_title VARCHAR(100) NOT NULL DEFAULT '',
+        agency_num INTEGER,
+        registration_date DATE NOT NULL
+      );
+    `);
+
+        this.log('Creating the employee records...');
+
+        await this.createEmployees(this.employees);
+
+        this.log('Database successfully initialized!');
+    }
+}
+
+module.exports = Database;
